72c1405e364aad99cf6c1bfe11f08c040fffe817
Ujjwal Verma <ujjwal.verma1905@gmail.com>
2026-06-17 14:08:10 +0530

    Update Content-Security-Policy to allow scripts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -21,7 +21,7 @@ const nextConfig: NextConfig = {
       {
         source: "/(.*)",
         headers: [
-          { key: "Content-Security-Policy", value: "default-src 'self'; img-src 'self' data:; style-src 'self' 'unsafe-inline'; script-src 'none';" },
+          { key: "Content-Security-Policy", value: "default-src 'self'; img-src 'self' data:; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval';" },
           { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
           { key: "X-Content-Type-Options", value: "nosniff" },
           { key: "X-Frame-Options", value: "DENY" },
